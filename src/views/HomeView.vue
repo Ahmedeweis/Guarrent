943131602690692320db8db7a29c670e03070349
Ahmedeweis <93427804+Ahmedeweis@users.noreply.github.com>
2026-02-28 17:38:14 +0200

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,15 @@
 <template>
   <div class="relative min-h-screen">
-    <Header />
+
     <main>
-      <HeroSection />
-      <AboutUsSection />
-      <DnaSection />
-      <DoctorsSection />
-      <DepartmentsSection />
-      <BlogSection />
-      <AppointmentSection />
+
     </main>
-    <FooterSection />
+
   </div>
 </template>
 
 <script setup>
-import Header from '../components/Header.vue';
-import HeroSection from '../components/HeroSection.vue';
-import AboutUsSection from '../components/AboutUsSection.vue';
-import DnaSection from '../components/DnaSection.vue';
-import DoctorsSection from '../components/DoctorsSection.vue';
-import DepartmentsSection from '../components/DepartmentsSection.vue';
-import BlogSection from '../components/BlogSection.vue';
-import AppointmentSection from '../components/AppointmentSection.vue';
-import FooterSection from '../components/FooterSection.vue';
+
 </script>
 
 <style scoped>
